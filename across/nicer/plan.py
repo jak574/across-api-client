aa77755c6e0b838ecfc221546d69dbecbb5f55ca
Jamie Kennea <jak51@psu.edu>
2023-10-18 08:04:50 -0700

    Allow length in NICER Plan

--- across/nicer/plan.py
+++ across/nicer/plan.py
@@ -1,10 +1,12 @@
 from ..base import ACROSSBase
 from ..coords import CoordSchema
 from ..user import UserArgSchema
-from ..daterange import DateRangeSchema, ACROSSDateRange
+
+# from ..daterange import DateRangeSchema, ACROSSDateRange
 from ..jobstatus import JobStatus, JobStatusSchema
 from marshmallow import fields, post_load, Schema
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
+from ..functions import convert_timedelta, convert_to_dt
 from datetime import datetime
 from typing import Optional
 from ..daterange import DateTime
@@ -59,20 +61,25 @@ class PlanSchema(Schema):
 
 
 @dataclass
-class Plan(ACROSSBase, ACROSSDateRange):
-    _schema = PlanSchema()
-    _arg_schema = PlanArgSchema()
-    _mission = "NICER"
-    _api_name = "Plan"
-
-    ra: Optional[float] = None
-    dec: Optional[float] = None
+class Plan(ACROSSBase):
+    length: InitVar[Optional[float]] = None
     begin: Optional[datetime] = None
     end: Optional[datetime] = None
+    ra: Optional[float] = None
+    dec: Optional[float] = None
     radius: Optional[float] = None
     obsid: Optional[int] = None
     targetid: Optional[int] = None
     entries: list[PlanEntry] = field(default_factory=list)
 
+    _schema = PlanSchema()
+    _arg_schema = PlanArgSchema()
+    _mission = "NICER"
+    _api_name = "Plan"
+
     # JobStatus
     status: JobStatus = JobStatus()
+
+    def __post_init__(self, length):
+        if self.begin is not None and length is not None:
+            self.end = convert_to_dt(self.begin) + convert_timedelta(length)
