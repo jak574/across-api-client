cd2638b64f0942935dffd93c57be5e9b3042a71e
Jamie Kennea <jak51@psu.edu>
2023-11-19 09:03:59 -0500

    add docstring

--- across_client/swift/visibility.py
+++ across_client/swift/visibility.py
@@ -8,6 +8,44 @@ from .constants import MISSION
 
 
 class SwiftVisibility(ACROSSBase, ACROSSResolveName, ACROSSDateRange):
+    """
+    Class representing the visibility of Swift objects.
+
+    Parameters:
+    ----------
+    ra : float
+        Right ascension of the object.
+    dec : float
+        Declination of the object.
+    begin : datetime
+        Start time of the visibility period.
+    end : datetime
+        End time of the visibility period.
+    hires : bool, optional
+        Flag indicating whether high-resolution data is requested. Default is True.
+    entries : list
+        List of entries.
+
+    Attributes:
+    ----------
+    _mission : str
+        Mission name.
+    _api_name : str
+        API name.
+    _schema : VisibilitySchema
+        Schema for visibility.
+    _get_schema : VisibilityGetSchema
+        Schema for getting visibility.
+    status : JobInfo
+        Job information.
+
+    Methods:
+    -------
+    __init__(self, **kwargs)
+        Initializes a new instance of the SwiftVisibility class.
+
+    """
+
     # Type hints
     ra: float
     dec: float
