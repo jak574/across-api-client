837f5927a9496e00931b125ac24531313d114711
Jamie Kennea <kennea@swift.psu.edu>
2023-12-13 10:19:42 -0500

    Update to current AWS test location

--- across_client/constants.py
+++ across_client/constants.py
@@ -5,5 +5,5 @@ API_URL: str
     The base URL of the ACROSS API.
 """
 
-# API_URL = "https://3pipyptgydz2uskazmvkq64et40rcrcz.lambda-url.us-east-1.on.aws/"
-API_URL = "http://localhost:3333/labs/api/v1/"
+API_URL = "https://3pipyptgydz2uskazmvkq64et40rcrcz.lambda-url.us-east-1.on.aws/"
+# API_URL = "http://localhost:3333/labs/api/v1/"
