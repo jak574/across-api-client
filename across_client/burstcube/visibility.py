bdca548d583c04580cbcd190f743ad84020e9c14
Jamie Kennea <kennea@swift.psu.edu>
2023-12-13 11:27:28 -0500

    Make auto-GET

--- across_client/burstcube/visibility.py
+++ across_client/burstcube/visibility.py
@@ -61,6 +61,9 @@ class BurstCubeVisibility(ACROSSBase, ACROSSResolveName, ACROSSDateRange):
         self.status = JobInfo()
         for k, a in kwargs.items():
             setattr(self, k, a)
+        # As this is a GET only class, we can validate and get the data
+        if self.validate_get():
+            self.get()
 
 
 # Alias
