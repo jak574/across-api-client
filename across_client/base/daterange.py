d6133e3734ed2ed966d27f4c11071819959d5f54
Jamie Kennea <jak574@gmail.com>
2024-03-14 06:06:32 -0400

    Remove length

--- across_client/base/daterange.py
+++ across_client/base/daterange.py
@@ -1,7 +1,5 @@
-from datetime import datetime, timedelta
-from typing import Optional, Union
-
-from ..functions import convert_timedelta, convert_to_dt  # type: ignore
+from datetime import datetime
+from typing import Optional
 
 
 class ACROSSDateRange:
@@ -36,15 +34,3 @@ class ACROSSDateRange:
 
     begin: datetime
     end: Optional[datetime]
-    _length: Union[float, timedelta, str, None]
-
-    @property
-    def length(self):
-        return self._length
-
-    @length.setter
-    def length(self, td: Union[float, timedelta, str, None]):
-        self._length = td
-        if hasattr(self, "begin") and self.begin is not None:
-            if hasattr(self, "end") is False or self.end is not None:
-                self.end = convert_to_dt(self.begin) + convert_timedelta(td)
